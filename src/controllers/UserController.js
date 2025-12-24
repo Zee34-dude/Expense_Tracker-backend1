62e49128f9ce38c2009e34419eaabd148355e809
zion ubesie <zionubesie@gmail.com>
2025-12-24 08:46:57 +0100

    push

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,8 +6,8 @@ class UserController {
   // GET /api/users
   async getUsers(req, res) {
     try {
-      const snapshot = await db.collection("users").get();
-      const users = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      const users = await prisma.user.findMany()
+      
       res.status(200).json(users);
     } catch (error) {
       res.status(500).json({ error: error.message });
